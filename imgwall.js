84e13076489ce3d0c4ffaec29d7c750390978fc2
AzusaOu <happy_panda@126.com>
2018-02-11 23:40:16 +0800

    Add files via upload

--- imgwall.js
+++ imgwall.js
@@ -7,15 +7,12 @@ function getHtml(url, async=false) {
 	xmlHttp.open("GET", url, async);
 	xmlHttp.send(null);
 	return xmlHttp.responseText
-
-
 }
 
 function selectorGen() {
 
 	var pics = getHtml('itemList.json')
-	//console.log(pics)
 	return JSON.parse(pics)
+	// return $.getJSON('./itemList.json').responseJSON
 
 }
-
--- iwGen.py
+++ iwGen.py
@@ -1,29 +1,47 @@
 # -*- coding: utf-8 -*-
 import os
+import shutil
 import json
+from PIL import Image
 
 import azLib as al
 
+DIR_THUMB = 'thumb'
+
+def imgCompress(fname, sname):
+	img = Image.open(fname)
+	img.thumbnail((128, 128))
+	img.save(sname, 'PNG')
+
 def fileStructure():
+	
 	fo = al.FileOperation()
-	fileLst = fo.fileLstMaker('.', ['.jpg', '.jpeg', '.gif', '.png'])
+	fileLst = fo.fileLstMaker('.', ['.jpg', '.jpeg', '.gif', '.png', '.JPG', '.PNG'])
 	classified = {}
 
 	currentFolder = ''
 	for p in fileLst:
-		folder = os.path.split(p)[0]
+		tmp = os.path.split(p)
+		# print tmp
+		fname = tmp[-1]
+		folder = tmp[0]
+		imgCompress(p, './%s/'%DIR_THUMB + fname)
 		if folder == currentFolder:
-			classified[folder.replace('\\', '/')].append(p.replace('\\', '/'))
+			classified[folder.replace('\\', '/')].append([p.replace('\\', '/'), fname])
 		else:
-			classified[folder.replace('\\', '/')] = [p.replace('\\', '/')]
+			classified[folder.replace('\\', '/')] = [[p.replace('\\', '/'), fname]]
 		currentFolder = folder
 	return classified
 
 def fileLstWriteOut(fname):
+	try:
+		shutil.rmtree(DIR_THUMB)
+	except:
+		print('It seems that folder [%s] has not been created.' % DIR_THUMB)
+	os.mkdir(DIR_THUMB)
 	fs = fileStructure()
 	with open(fname,'w') as f:
 		f.write(json.dumps(fs))
-
 
 def main():
 	fileLstWriteOut('itemList.json')
